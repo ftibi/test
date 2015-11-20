65e0cd47a64752785df371f9363ea577a0c3a4f5
ftibi <thibault.folly@gmail.com>
2015-11-20 12:23:59 +0100

    update synthax

--- spinWords.js
+++ spinWords.js
@@ -1,19 +1,18 @@
 function spinWords(sentence){
 	var sentarr = sentence.split(" ");
-  var res = [];
-  for (var j = 0; j < sentarr.length; j++){
-  	if (sentarr[j].length >= 5){
-    	var newword = "";
-      for (var i = 0; i < sentarr[j].length; i++){
-      	newword += sentarr[j][sentarr[j].length - 1 - i];
-      }
-    	res.push(newword);
-    }
-    else {
-    	res.push(sentarr[j]);
-    }
-  }
-  return res.join(" ");
-}
+	var res = [];
+	for (var j = 0; j < sentarr.length; j++){
+		if (sentarr[j].length >= 5){
+			var newword = "";
+			for (var i = 0; i < sentarr[j].length; i++){
+				newword += sentarr[j][sentarr[j].length - 1 - i];
+			}
+			res.push(newword);
+		}
+		else {
+			res.push(sentarr[j]);
+		}
+		return res.join(" ");
+	}
 
-console.log(spinWords("salut les gens"));
+	console.log(spinWords("salut les gens"));
