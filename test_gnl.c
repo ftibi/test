edd211fdb39107da078781ab09f90cf4c198feba
tfolly <tfolly@student.42.fr>
2016-01-19 16:06:45 +0100

    corr testgnl

--- test_gnl.c
+++ test_gnl.c
@@ -1,12 +1,14 @@
 #include "get_next_line.h"
 #include <stdio.h>
-#include <fcntl.h>a
+#include <fcntl.h>
 
 int	main(int ac, char **av)
 {
 	int	fd;
 	char *line;
 
+	ac = 0;
+	fd = open(av[1], O_RDONLY);
 	printf("%d : %s\n", get_next_line(fd, &line), line);
 	printf("%d : %s\n", get_next_line(fd, &line), line);
 	
