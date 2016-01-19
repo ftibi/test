13e308a86aefb8dbb6f5beb06a986bb97667fbc4
tfolly <tfolly@student.42.fr>
2016-01-19 13:35:33 +0100

    add testgnl

---
+++ test_gnl.c
@@ -0,0 +1,13 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>a
+
+int	main(int ac, char **av)
+{
+	int	fd;
+	char *line;
+
+	printf("%d : %s\n", get_next_line(fd, &line), line);
+	
+	return (0);
+}
