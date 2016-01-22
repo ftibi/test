b3046abb0c0bc7cb1f18a3ae9fe0c7d2fbfee00a
tfolly <tfolly@student.42.fr>
2016-01-22 13:05:54 +0100

    test

--- test_gnl.c
+++ test_gnl.c
@@ -6,11 +6,14 @@ int	main(int ac, char **av)
 {
 	int	fd;
 	char *line;
-
+    int ret = 1;
 	ac = 0;
 	fd = open(av[1], O_RDONLY);
-	printf("%d : %s\n", get_next_line(fd, &line), line);
-	printf("%d : %s\n", get_next_line(fd, &line), line);
 	
+	while (ret)
+	{
+	    ret = get_next_line(fd, &line);
+	    printf("%d : %s\n", ret, line);
+	}
 	return (0);
 }
